c11bc870717cac4a2f681159c858e22d9c86ad8b
Paul Mendoza <paulmndza19@gmail.com>
2024-04-25 22:57:56 +0800

    fix: add user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(
+  user_type: "Admin",
+  email: "testuser@gmail.com",
+  first_name: "Test",
+  last_name: "Test",
+  password: "password",
+  birthday: "12/12/2000"
+)
