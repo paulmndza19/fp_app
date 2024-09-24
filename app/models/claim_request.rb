3b28d58ff05a81312f27ebd80d00256e9c092f2d
crlxltbr <crlxltbr@EMAIL.com>
2024-09-24 23:21:44 +0800

    Added claim request conditions

--- app/models/claim_request.rb
+++ app/models/claim_request.rb
@@ -1,7 +1,7 @@
 class ClaimRequest < ApplicationRecord
   has_paper_trail
   acts_as_paranoid
-  
+
   paginates_per 10
 
   belongs_to :user
@@ -13,6 +13,8 @@ class ClaimRequest < ApplicationRecord
   has_one_attached :document
 
   validates :document, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'], on: :create
+  validate :one_claim_per_year, on: :create
+  validate :sufficient_contributions, on: :create
 
   def type
     claim_request_type.name
@@ -37,13 +39,13 @@ class ClaimRequest < ApplicationRecord
     #   end
     # end
 
-    # # Check if the member already received a claim in the current year
-    # def one_claim_per_year
-    #   current_year_claims = ClaimRequest.where(user_id: user.id, created_at: Time.current.beginning_of_year..Time.current.end_of_year)
-    #   if current_year_claims.exists?
-    #     errors.add(:base, "You have already received a claim this year. You can only request another claim next year.")
-    #   end
-    # end
+    # Check if the member already received a claim in the current year
+    def one_claim_per_year
+      has_request_within_this_year = ClaimRequest.where(user_id: user_id, status: "Approved", updated_at: 1.year.ago..Time.zone.now).exists?
+      if has_request_within_this_year
+        errors.add(:base, "You have already received a claim this year. You can only request another claim next year.")
+      end
+    end
 
     # # Check if the member has unpaid contribution dues
     # def no_unpaid_dues
@@ -52,15 +54,11 @@ class ClaimRequest < ApplicationRecord
     #   end
     # end
 
-    # # Check if the member has made enough contributions
-    # def sufficient_contributions
-    #   if user.contributions_count < required_contributions_count
-    #     errors.add(:base, "You need to make more contributions before you can request a claim.")
-    #   end
-    # end
-
-    # # Define the required number of contributions (adjust as necessary)
-    # def required_contributions_count
-    #   5 # Example: require at least 5 contributions to be eligible
-    # end
+    # Check if the member has made enough contributions
+    def sufficient_contributions
+      contributions_within_this_year = Contribution.where(user_id: user_id, updated_at: 1.year.ago.beginning_of_month..Time.zone.now).count
+      if contributions_within_this_year < 12
+        errors.add(:base, "You need to make #{12 - contributions_within_this_year} more contributions before you can request a claim.")
+      end
+    end
 end
